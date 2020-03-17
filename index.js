aaf68d81cfdb8b7a8d9806523ade6ef6a116ca41
Mohammed Assellalou <mohammed.assellalou.official@gmail.com>
2020-03-17 18:21:02 +0100

    basic files

---
+++ index.js
@@ -0,0 +1,37 @@
+const Discord = require('discord.js');
+const {prefix, token, giphyToken} = require('./config.json');
+const client = new Discord.Client();
+const giphy = require('giphy-api')(giphyToken);
+
+client.once('ready', ()=>{
+    console.log('Ready!');
+    
+});
+
+client.on('message', message=>{
+    
+    //console.log(message.content);
+    if(message.content.startsWith(`${prefix}kick`)){
+        //message.channel.send('hello from bot');
+        if(message.member.hasPermission(['ADMINISTRATOR'])){
+            let member = message.mentions.members.first();
+            member.kick().then((member)=>{
+                giphy.search({q:'fail', rating:'g'}).then((res)=>{
+                    let tr = res.data.length;
+                    let ri = Math.floor(Math.random()*10+1)%tr;
+                    let fr = res.data[ri];
+                    message.channel.send(`Suck it ${member.displayName}`, {
+                        files:[fr.images.fixed_height.url]
+                    });
+                }).catch(()=>{
+                    message.channel.send('Gifs are not working!');
+                })
+            }).catch(()=>{
+                    message.channel.send('Something went wrong!');
+                })
+        }else{
+            message.channel.send('You are not permitted to do this, sorry!');
+        }
+    }
+})
+client.login(token);
